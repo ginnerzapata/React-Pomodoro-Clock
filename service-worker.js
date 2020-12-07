9ca8e8495d38421e91ef32561321e96eaf55eb92
Ginner Zapata <ginnerzapata@gmail.com>
2020-12-07 05:33:04 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ginnerzapata.github.io/React-Pomodoro-Clock/precache-manifest.59d4108398a27118ffaa8787e0435736.js"
+  "/ginnerzapata.github.io/react-pomodoro-clock/precache-manifest.ab435b4148dc00a14b021112509ddcde.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ginnerzapata.github.io/React-Pomodoro-Clock/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ginnerzapata.github.io/react-pomodoro-clock/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
