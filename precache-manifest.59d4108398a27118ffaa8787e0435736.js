60cda4800022c8803aa1e48ecef34aa1e8c8b661
Ginner Zapata <ginnerzapata@gmail.com>
2020-12-07 05:26:28 -0300

    Updates

---
+++ precache-manifest.59d4108398a27118ffaa8787e0435736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20cec7d9a6428fd92f132e2998dc6f2",
+    "url": "/ginnerzapata.github.io/React-Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "0e0d0efab67272cb2e29",
+    "url": "/ginnerzapata.github.io/React-Pomodoro-Clock/static/css/main.540a1c28.chunk.css"
+  },
+  {
+    "revision": "d06aa85e6cde01125914",
+    "url": "/ginnerzapata.github.io/React-Pomodoro-Clock/static/js/2.218f4da8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ginnerzapata.github.io/React-Pomodoro-Clock/static/js/2.218f4da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e0d0efab67272cb2e29",
+    "url": "/ginnerzapata.github.io/React-Pomodoro-Clock/static/js/main.7f67161d.chunk.js"
+  },
+  {
+    "revision": "b7286784a8d254c7c130",
+    "url": "/ginnerzapata.github.io/React-Pomodoro-Clock/static/js/runtime-main.fe858696.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ginnerzapata.github.io/React-Pomodoro-Clock/precache-manifest.59d4108398a27118ffaa8787e0435736.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ginnerzapata.github.io/React-Pomodoro-Clock/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
